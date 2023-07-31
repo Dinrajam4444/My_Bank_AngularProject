fa10d50867572a17ae2b1b8b34809b834ea9c10a
Dinraj AM <dinrajrockzz444@gmail.com>
2023-07-31 20:02:31 +0530

    first commit of my angular bank project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { StatementComponent } from './statement/statement.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    StatementComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
