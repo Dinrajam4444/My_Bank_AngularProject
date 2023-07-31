cfbdc90fb28677162b1c94c34ce0274a8da9c224
Dinraj AM <dinrajrockzz444@gmail.com>
2023-07-31 21:56:50 +0530

    second commit of my angular bank project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { StatementComponent } from './statement/statement.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  data="Happy banking with us.."
+  pdata="Enter Acno"
+
+  acno:any=""
+  psw:any=""
+
+  constructor(private rout2:Router) { }
+
+  ngOnInit(): void{
+  }
+
+  // login(a:any,b:any){
+
+  //   // alert("login clicked")
+
+  //   console.log(a.value);
+  //   console.log(b.value);
+    
+  // }
+
+
+  login(){
+
+    // alert("login clicked")
+
+    // this.acno=a.value
+    // this.psw=b.value
+    console.log(this.acno);
+    console.log(this.psw);
+
+    this.rout2.navigateByUrl('home')
+    
+  }
+
+  // acnoChange(event:any){
+
+  //   // console.log(event.target.value);
+  //   this.acno=event.target.value
+  //   console.log(this.acno);
+    
+  // }
+
+  // acnoPassword(event:any){
+
+  //   // console.log(event.target.value);
+  //   this.psw=event.target.value
+  //   console.log(this.psw)  
+    
+  // }
 
 }
